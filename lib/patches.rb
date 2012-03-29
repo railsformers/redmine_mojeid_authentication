ae58bbd08225ba37a6b493ecdf794aff70fcf88d
Richard Říman <riman.richard@gmail.com>
2012-03-29 16:22:43 +0200

    fixes bad validation

--- lib/patches.rb
+++ lib/patches.rb
@@ -3,7 +3,7 @@ require_dependency 'user'
 module UserPatch
   def self.included(base) # :nodoc:
     base.class_eval do
-      validates_uniqueness_of :mojeid_identity_url
+      validates_uniqueness_of :mojeid_identity_url, :allow_nil => true
     end
   end
 end
